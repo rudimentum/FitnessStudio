5ea2d25d6e1e62f2a6113996265d5dfb1ebcd533
Victoria Grinberg <31312528+rudimentum@users.noreply.github.com>
2022-02-12 00:00:24 +0300

    Setup log in

--- app/src/main/java/com/rudimentum/fitnessstudio/LoginActivity.kt
+++ app/src/main/java/com/rudimentum/fitnessstudio/LoginActivity.kt
@@ -2,15 +2,17 @@ package com.rudimentum.fitnessstudio
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
 
-class LoginActivity : AppCompatActivity() {
-        lateinit var userEmail : EditText
-        lateinit var userPass : EditText
-        lateinit var signInButton: Button
+class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +24,50 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
             startActivity(intent)
         }
+
+        btnLogin.setOnClickListener {
+            validateLoginDetails()
+        }
     }
+
+    private fun validateLoginDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(editTextLoginEmail.text.toString().trim { it <= ' '} ) -> {
+                showErrorSnackBar(resources.getString(R.string.error_msg_email), true)
+                false
+            }
+            TextUtils.isEmpty(editTextLoginPassword.text.toString().trim { it <= ' '} ) -> {
+                showErrorSnackBar(resources.getString(R.string.error_msg_password), true)
+                false
+            }
+            else -> {
+                showErrorSnackBar(resources.getString(R.string.sign_in_successful), false)
+                val email = editTextLoginEmail.text.toString().trim { it <= ' '}
+                val password = editTextLoginPassword.text.toString().trim { it <= ' '}
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+
+                            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                            // rid of stack
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            intent.putExtra("user_id", FirebaseAuth.getInstance().currentUser!!.uid)
+                            intent.putExtra("email_id", email)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            Toast.makeText(
+                                this@LoginActivity,
+                                task.exception!!.message.toString(),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                true
+            }
+        }
+    }
+
+
 }

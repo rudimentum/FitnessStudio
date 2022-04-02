d3839d82cadd05da99f54eda21efce12aae2bc6b
Victoria Grinberg <31312528+rudimentum@users.noreply.github.com>
2022-04-02 13:34:22 +0300

    Set up firestore

---
+++ app/src/main/java/com/rudimentum/fitnessstudio/firestore/FirestoreClass.kt
@@ -0,0 +1,8 @@
+package com.rudimentum.fitnessstudio.firestore
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+class FirestoreClass {
+
+    private val mFireStoreClass = FirebaseFirestore.getInstance()
+}
